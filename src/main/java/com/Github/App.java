155e90378d598f6ed2733424b6a99cfe7025f9de
Ramya S <ramya.s@ilantus.com>
2024-05-08 12:28:00 +0530

    Create Github

---
+++ src/main/java/com/Github/App.java
@@ -0,0 +1,13 @@
+package com.Github;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/Github/Home.java
@@ -0,0 +1,8 @@
+package com.Github;
+
+public class Home {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
